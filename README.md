91faac814a5bdeec71bc2c93894277184bab956a
breakerthb <breakerthb@126.com>
2017-04-25 06:40:16 +0000

    Add 1-2

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ProgramingHandByHand
 C/C++ programing practice
 
-[天花板编程手把手计划-第1期-第1天](http://www.jianshu.com/p/da02a9e32810)
+![](http://upload-images.jianshu.io/upload_images/145902-813936ab7b282e90.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240/format/jpg)
+
+[天花板编程手把手计划-第1期-第2天](http://www.jianshu.com/p/bc9092eff51b)
 
 source.cpp
 
--- source.cpp
+++ source.cpp
@@ -1,60 +1,71 @@
-int IsPrimeNum1(int num)
+#include <stdio.h>
+
+#define SIZE 3
+
+int g_arr[SIZE][SIZE] = 
+    {    3, 4, 9,
+        6, 2, 7,
+        5, 1, 8
+    };
+    
+void PrintfMatrix(int arr[][SIZE])
 {
-    int i;
-
-    if (num == 1)
+    int i, j;
+    for (i = 0; i < SIZE; i++)
     {
-        return 0;
-    }
-
-    for (i = 2; i < num; i++)
-    {
-        if (num % i == 0)
+        for (j = 0; j < SIZE; j++)
         {
-            return 0;
+            printf("%5d", arr[i][j]);
         }
+        printf("\n");
     }
-
-    return 1;
 }
 
-int IsPrimeNum2(int num)
+void Sort(int* pArr, int num)
 {
-    int i, k;
-
-    if (num == 1)
+    int t;
+    for (int i = 0; i < num - 1; i++)
     {
-        return 0;
-    }
-
-    k = (int)sqrt((double)num);
-
-    for (i = 2; i <= k; i++)
-    {
-        if (num % i == 0)
+        for (int j = i + 1; j < num; j++)
         {
-            return 0;
+            if (pArr[i] > pArr[j])
+            {
+                t = pArr[i];
+                pArr[i] = pArr[j];
+                pArr[j] = t;
+            }
+
         }
     }
+}
 
-    return 1;
+int CalculateSum(int arr[][SIZE])
+{
+    int sum = 0;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        sum += arr[i][i];
+        arr[i][i] = 0;
+
+        sum += arr[i][SIZE - 1 - i];
+    }
+
+    return sum;
 }
 
 int main()
 {
-    int num, i;
-    int isPrimeNum;
-    for (num = 1; num <= 100; num++)
-    {
-        if (IsPrimeNum1(num) == 1)
-        //if (IsPrimeNum2(num) == 1)
-        {
-            printf("%d\n", num);
-        }
-        else
-        {
-            printf("%d ", num);
-        }
-    }
-    printf("\n");
+    int ret;
+
+    printf("原始矩阵：\n");
+    PrintfMatrix(g_arr);
+
+    Sort((int*)g_arr, SIZE * SIZE);
+    printf("排序后矩阵：\n");
+    PrintfMatrix(g_arr);
+
+    printf("对角线之和：\n");
+    ret = CalculateSum(g_arr);
+    printf("%d\n", ret);
 }
