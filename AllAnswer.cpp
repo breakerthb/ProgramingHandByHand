0ddc2e1b9eddd6faa38b681171d82226c0919df9
breakerthb <breakerthb@126.com>
2017-05-02 01:30:46 +0000

    Add 1-5 code

---
+++ AllAnswer.cpp
@@ -0,0 +1,97 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 3
+
+int g_arr[MAX_SIZE][MAX_SIZE] = { 0 };
+
+int Calculate()
+{
+	int i, j;
+	int sumLine, sumCol, sumDL1, sumDL2;
+	sumDL1 = 0;
+	sumDL2 = 0;
+	for (i = 0; i < MAX_SIZE; i++)
+	{
+		sumLine = 0;
+		sumCol = 0;
+
+		for (j = 0; j < MAX_SIZE; j++)
+		{
+			sumLine += g_arr[i][j];
+			sumCol += g_arr[j][i];
+		}
+
+		if (sumLine != 15 || sumCol != 15)
+		{
+			return 0;
+		}
+
+		sumDL1 += g_arr[i][i];
+		sumDL2 += g_arr[i][MAX_SIZE - 1 - i];
+	}
+
+	if (sumDL1 != 15 || sumDL2 != 15)
+	{
+		return 0;
+	}
+
+	return 1;
+}
+
+void PrintMap()
+{
+	int i, j;
+	for (i = 0; i < MAX_SIZE; i++)
+	{
+		for (j = 0; j < MAX_SIZE; j++)
+		{
+			printf("%3d ", g_arr[i][j]);
+		}
+
+		printf("\n");
+	}
+
+	printf("\n");
+}
+
+int g_cnt = 0;
+void FillBlank(int num)
+{
+	int i;
+	int nBak;
+	int* pArr = (int*)g_arr;
+
+	if (num <= 0)
+	{
+		if (Calculate() == 1)
+		{
+			PrintMap();
+		}
+
+		printf("%d\r", g_cnt++);
+
+		return;
+	}
+
+	for (i = 0; i < MAX_SIZE * MAX_SIZE; i++)
+	{
+		if (num >= pArr[i])
+		{
+			nBak = pArr[i];
+			pArr[i] = num;
+			FillBlank(num - 1);
+			pArr[i] = nBak;
+		}
+	}
+}
+
+int main()
+{
+
+	FillBlank(9);
+
+	return 0;
+}
+
--- README.md
+++ README.md
@@ -3,17 +3,12 @@ C/C++ programing practice
 
 ![](http://upload-images.jianshu.io/upload_images/145902-8eb2a909aefac2e9.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240/format/jpg)
 
-[天花板编程手把手计划-第1期-第4天](http://www.jianshu.com/p/cc312e23ed69)
+[天花板编程手把手计划-第1期-第5天]()
 
-- 斐波那契数列简单解法
+- 打印所有答案
 
-Fib1.cpp
+AllAnswer.cpp
 
-- 斐波那契数列递归解法
+- 打印第一个答案
 
-Fib2.cpp
-
-- 丢硬币的递归算法
-
-ThrowCoins.cpp
-
+FirstAnswer.cpp
